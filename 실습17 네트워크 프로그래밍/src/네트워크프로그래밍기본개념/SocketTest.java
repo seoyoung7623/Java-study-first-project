5329edf6d3e6b435a4fb9461cd3a697fa2bee3a0
Seoyoung Yoon <seoyoung7623@naver.com>
2022-07-25 01:54:21 +0900

    2022.07.25

--- 실습17 네트워크 프로그래밍/src/네트워크프로그래밍기본개념/SocketTest.java
+++ 실습17 네트워크 프로그래밍/src/네트워크프로그래밍기본개념/SocketTest.java
@@ -6,15 +6,18 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class SocketTest {
-	public static void main(String[] args) throws IOException{
-		try (Socket s = new Socket("time.c.nist.gov",13)){
+	public static void main(String[] args) throws IOException {
+		try (Socket s = new Socket("time-c.nist.gov", 13)) {
 			InputStream inStream = s.getInputStream();
 			Scanner in = new Scanner(inStream);
+			//Scanner 클래스는 기본 데이터차입/문자열을 구문분석할 수 있음.
+			//-> 다양한 타입의 입력값들을 읽어들이기 위한 편리한 방법을 제공하는 클래스.
+
+			
+			while (in.hasNextLine()) {
+				String line = in.nextLine();
+				System.out.println(line);
+			}
 		}
-		
-//		while(in.hasNextLine()) {
-//			String line = in.nextLine();
-//			System.out.println(line);
-//		}
 	}
 }
