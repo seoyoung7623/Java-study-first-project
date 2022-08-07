d034fa4bf6d9905992d4b1ab461ab3e61c959fb2
Seoyoung Yoon <seoyoung7623@naver.com>
2022-08-07 23:28:56 +0900

    2022.08.07

--- 실습 로그인,채팅프로그램/src/Login/ChattingClient.java
+++ 실습 로그인,채팅프로그램/src/Login/ChattingClient.java
@@ -1,5 +1,16 @@
 package Login;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ChattingClient {
+	Socket socket;
+	PrintWriter in;
+	BufferedReader out;
 	
+	public ChattingClient() {
+		// TODO Auto-generated constructor stub
+	}
 }
